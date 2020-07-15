ed4edea1260ec5e78eca1c338a898d49bf81df28
Damian de Vega <damiandevega@gmail.com>
2020-07-15 12:47:48 -0400

    adding action creators

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // Redux imports
 import { connect } from 'react-redux';
-import * as actionTypes from '../../store/actions';
+import * as actionCreators from '../../store/actions/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -82,26 +82,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
-    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-    onAddCounter: () =>
-      dispatch({
-        type: actionTypes.ADD,
-        payload: {
-          value: 10,
-        },
-      }),
-    onSubtractCounter: () =>
-      dispatch({
-        type: actionTypes.SUBTRACT,
-        payload: {
-          value: 15,
-        },
-      }),
-    onStoreResult: (result) =>
-      dispatch({ type: actionTypes.STORE_RESULT, result: result }),
-    onDeleteResult: (id) =>
-      dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id }),
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(10)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+    onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id)),
   };
 };
 
---
+++ src/store/actions/actions.js
@@ -0,0 +1,50 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBTRACT = 'SUBTRACT';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  }
+}
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  }
+}
+
+export const add = (value) => {
+  return {
+    type: ADD,
+    payload: {
+      value: value
+    }
+  }
+}
+
+export const subtract = (value) => {
+  return {
+    type: SUBTRACT,
+    payload: {
+      value: value
+    }
+  }
+}
+
+export const storeResult = (result) => {
+  return {
+    type: STORE_RESULT,
+    result: result
+  }
+}
+
+export const deleteResult = (resultElId) => {
+  return {
+    type: DELETE_RESULT,
+    resultElId: resultElId
+  }
+}
